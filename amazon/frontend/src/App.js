b1a29fe51cf633170b42aca59b3c6944e53ac76d
Abida <fowzia.abida21@gmail.com>
2021-08-16 16:35:24 +0200

    3rd commit

--- amazon/frontend/src/App.js
+++ amazon/frontend/src/App.js
@@ -23,45 +23,16 @@ function App() {
             <div className="row center">
               {
                 data.products.map(product => (
-                  <div key = {product._id}className="card">
-                  <a href={`/product/${product._id}`}>
-                   
-                    <img 
-                    className="medium" 
-                    src={product.image} 
-                    alt={product.name} 
-                    />
-                  </a>
-                  <div className="card-body">
-                  <a href={`/product/${product._id}`}>
-                      <h2>{product.name}</h2>
-                    </a>
-                    <div className="rating">
-                      <span> <i className="fa fa-star"></i> </span>
-                      <span> <i className="fa fa-star"></i> </span>
-                      <span> <i className="fa fa-star"></i> </span>
-                      <span> <i className="fa fa-star"></i> </span>
-                      <span> <i className="fa fa-star-0"></i> </span>
-                    </div>
-                    <div className="price">${product.price}</div>
-                  </div>
-                </div>
-                  
+                  <Product key= {product._id}   product = {product}></Product>
 
-                ))
-              }
+                ))}
              
-              
-              
-             
-           
             </div>
           </div>
 
     </main>
-    <footer>
-        All Rights Reserved.
-
+    <footer className="row center"> All right reserved
+        
     </footer>
 </div>
     
