30af7bd7369b4c347dce942b268b31c221132516
Abida <fowzia.abida21@gmail.com>
2021-08-18 00:31:17 +0200

    error fix

--- amazon/frontend/src/App.js
+++ amazon/frontend/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-import Product from "./components/Product";
-import data from './data';
+import {BrowserRouter, Route} from "react-router-dom"
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
 
 function App() {
+  
   return (
+    <BrowserRouter>
     <div classNameName="grid_container">
     <header className="row">
     
@@ -20,24 +24,15 @@ function App() {
         
     </header>
     <main>
-        <div>
-            <div className="row center">
-              {
-                data.products.map(product => (
-                  <Product key= {product._id}   product = {product}></Product>
-
-                ))}
-             
-            </div>
-          </div>
+      <Route path= "/product/:id" component = {ProductScreen}></Route>
+      <Route path = "/" component = {HomeScreen} exact></Route>
+        
 
     </main>
-    <footer className="row center"> All right reserved
-        
-    </footer>
+    <footer className="row center"> All right reserved  </footer>
 </div>
-    
-  );
+</BrowserRouter> 
+ );
 }
-
+ 
 export default App;
